4abafed530cec392813b4f7f7a2f6b96445e6cca
MINGKYME <mingkyme@gmail.com>
2026-09-23 09:30:03 +0900

    feat: add XML sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://food.mingky.me',
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
